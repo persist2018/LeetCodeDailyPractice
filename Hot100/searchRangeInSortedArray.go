ef69dd5f1c24865f452afd2a2c7663762ba1f3bf
persistchow <1606441033@qq.com>
2022-05-21 11:27:51 +0800

    slove searchRange by search LeftBound and Right Bound

--- Hot100/searchRangeInSortedArray.go
+++ Hot100/searchRangeInSortedArray.go
@@ -1,1 +1,53 @@
 package main
+
+// https://leetcode.cn/problems/find-first-and-last-position-of-element-in-sorted-array/
+
+func searchRange(nums []int, target int) []int {
+	if len(nums) == 0 {
+		return []int{-1, -1}
+	}
+	leftBound := searchLeftBoundInSortedArray(nums, 0, len(nums)-1, target)
+	rightBound := searchRightBoundInSortedArray(nums, 0, len(nums)-1, target)
+	if leftBound > len(nums) || rightBound > len(nums) {
+		return []int{-1, -1}
+	}
+	if nums[leftBound] == target && nums[rightBound] == target {
+		return []int{leftBound, rightBound}
+	}
+	return []int{-1, -1}
+}
+
+// change tradtioinal binary search
+func searchLeftBoundInSortedArray(nums []int, left, right, target int) (index int) {
+	var mid int
+	for left <= right {
+		mid = left - (left-right)/2
+		if nums[mid] == target {
+			right = mid - 1
+		} else {
+			if nums[mid] < target {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		}
+	}
+	return left
+}
+
+func searchRightBoundInSortedArray(nums []int, left, right, target int) (index int) {
+	var mid int
+	for left <= right {
+		mid = left - (left-right)/2
+		if nums[mid] == target {
+			left = mid + 1
+		} else {
+			if nums[mid] < target {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		}
+	}
+	return right
+}
