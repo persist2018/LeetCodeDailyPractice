4d187ab7fb9490bacd06155df89655dd138f5923
persistchow <1606441033@qq.com>
2022-05-22 16:16:37 +0800

    slove trappingRain by DP

---
+++ Hot100/trap.go
@@ -0,0 +1,38 @@
+package main
+
+// https://leetcode.cn/problems/trapping-rain-water/
+
+func trappingRainDP(height []int) (ans int) {
+	length := len(height)
+	if length == 0 {
+		return 0
+	}
+	leftMax := make([]int, length)
+	rightMax := make([]int, length)
+	// leftMax[i]=max(leftMax[i-1],hight[i])
+	for i := 0; i < length; i++ {
+		if i == 0 {
+			leftMax[i] = height[i]
+		} else {
+			leftMax[i] = max(height[i], leftMax[i-1])
+		}
+	}
+	// rightMax[i]=max(rightMax[i+1],hight[i])
+	for i := length - 1; i >= 0; i-- {
+		if i == length-1 {
+			rightMax[i] = height[i]
+		} else {
+			rightMax[i] = max(height[i], rightMax[i+1])
+		}
+	}
+	// rain[i]=min(leftMax[i],rightMax[i])-height[i]
+
+	for i, h := range height {
+		ans += min(leftMax[i], rightMax[i]) - h
+	}
+	return
+}
+
+func trappingRainTwoPointers(height []int) int {
+
+}
