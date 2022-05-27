4d398aac5e2ab5e0c54b80e08ed0a08332c69cbe
persistchow <1606441033@qq.com>
2022-05-27 23:34:13 +0800

    slove https://leetcode.cn/problems/sort-colors/

---
+++ Hot100/sortColors.go
@@ -0,0 +1,17 @@
+package main
+
+// https://leetcode.cn/problems/sort-colors/
+func swapColors(colors []int, target int) (countTarget int) {
+	for i, c := range colors {
+		if c == target {
+			colors[i], colors[countTarget] = colors[countTarget], colors[i]
+			countTarget++
+		}
+	}
+	return
+}
+
+func sortColors(nums []int) {
+	count0 := swapColors(nums, 0) // 把 0 排到前面
+	swapColors(nums[count0:], 1)  // nums[:count0] 全部是 0 了，对剩下的 nums[count0:] 把 1 排到前面
+}
