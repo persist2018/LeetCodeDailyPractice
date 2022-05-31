dc050ce1456696874cf70bf6f574b22eaf63dfba
persistchow <1606441033@qq.com>
2022-05-31 23:45:29 +0800

    slove // https://leetcode.cn/problems/palindrome-number/

---
+++ Hot100/longestConsecutive.go
@@ -0,0 +1,26 @@
+package main
+
+func longestConsecutive(nums []int) int {
+	numMap := map[int]bool{}
+	longest := 0
+	for _, num := range nums {
+		numMap[num] = true
+	}
+
+	for num := range numMap {
+		if !numMap[num-1] {
+			currentNum := num
+			currentLength := 1
+
+			for numMap[currentNum+1] {
+				currentNum++
+				currentLength++
+			}
+
+			if longest < currentLength {
+				longest = currentLength
+			}
+		}
+	}
+	return longest
+}
