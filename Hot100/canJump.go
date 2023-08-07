aa3439ecfb547a7c97bcd12f8095fc059fb4bc0f
persist2018 <1606441033@qq.com>
2023-08-07 16:14:21 +0800

    fix

--- Hot100/canJump.go
+++ Hot100/canJump.go
@@ -4,6 +4,7 @@ package main
 // This problem can be solved by greedy algorithm
 func canJump(nums []int) bool {
 	maxMost := 0
+
 	for i := 0; i < len(nums); i++ {
 		if maxMost >= i {
 			maxMost = max(maxMost, i+nums[i])
